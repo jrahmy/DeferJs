d9d0c6dd007f4d3b215a3c096b57b0bc92c6dc63
Jeremy P <jrahmy+github@gmail.com>
2015-04-05 23:02:17 -0700

    Resolve merge conflicts

--- FileSums.php
+++ FileSums.php
@@ -21,13 +21,13 @@ class FileSums
     /**
      * Provides an associative array of filenames to hashes.
      *
-     * @return array An associative array of filesums.
+     * @return array An associative array of filesums
      */
     public static function getHashes()
     {
         return [
-                'library/Jrahmy/DeferJs/Listener.php' => '8f7a41e1b4765828a5c7c316c083515d',
-                'library/Jrahmy/DeferJs/Deferrer.php' => 'a7a9af7718542c083f6866cdb0125509',
+                'library/Jrahmy/DeferJs/Listener.php' => '5b32645718c71b44df0bad694f9c0bd9',
+                'library/Jrahmy/DeferJs/Deferrer.php' => '9a3bff68f7f35490e1c59c1530282963',
 
         ];
     }
--- README.md
+++ README.md
@@ -17,7 +17,3 @@ License
 -------
 For the full copyright and license information, please view the LICENSE file
 that was distributed with this source code.
-
-**Copyright (c) 2014 Jeremy P**
-
-**Parts derived from XFOptimise by Luke Foreman**
