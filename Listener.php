7ee4bb5425f58e985d1f0ae1c6da1ad6a514d494
Jeremy P <jrahmy+github@gmail.com>
2016-03-31 20:33:21 -0700

    Update license information

--- Listener.php
+++ Listener.php
@@ -3,10 +3,10 @@
 /*
  * This file is part of a XenForo add-on.
  *
- * (c) Jeremy P <http://xenforo.com/community/members/jeremy-p.450/>
+ * (c) Jeremy P <https://xenforo.com/community/members/jeremy-p.450/>
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  */
 
 namespace Jrahmy\DeferJs;
@@ -14,7 +14,7 @@ namespace Jrahmy\DeferJs;
 /**
  * Provides static methods to extend the XenForo API.
  *
- * @author Jeremy P <http://xenforo.com/community/members/jeremy-p.450/>
+ * @author Jeremy P <https://xenforo.com/community/members/jeremy-p.450/>
  */
 class Listener
 {
